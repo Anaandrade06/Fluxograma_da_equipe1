9430f01c9a3e7c72ee866a6b398822bfb65afe90
fluxograma <anaelisaandrade65@gmail.com>
2024-07-26 11:04:13 -0300

    mais um dia

--- Modelos/Materia_Prima.cs
+++ Modelos/Materia_Prima.cs
@@ -1,4 +1,12 @@
 namespace Modelos;
 
-public class Materia_Prima 
-string 
+public class Materia_Prima
+{
+string nome; 
+int quantidade; 
+int Id_MateriaPrima;
+public void SetNome (string Nome)
+{
+this.nome = Nome;
+}
+}
